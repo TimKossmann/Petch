04b2914dfed954dbcedfa5fc9ac8f98c3901c6ee
tc020 <99870964+tc020@users.noreply.github.com>
2022-02-18 02:31:59 +0100

    Navigation verknüpft

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
       body: const Center(
-        child: Text('Startseite'),
+        child: Text('Petch', style: TextStyle(color: Colors.blue, fontSize: 80.0)),
       ),
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
@@ -43,26 +43,107 @@ class MyHomePage extends StatelessWidget {
               child: Text('Navigation'),
             ),
             ListTile(
-              title: const Text('Mein Profil'),
+              title: const Text('Login'),
               onTap: () {
                 // Update the state of the app
                 // ...
                 // Then close the drawer
-                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()),);
               },
             ),
             ListTile(
-              title: const Text('Einstellungen'),
+              title: const Text('Über Petch'),
               onTap: () {
                 // Update the state of the app
                 // ...
                 // Then close the drawer
-                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => FirstPage()),);
+              },
+            ),
+            ListTile(
+              title: const Text('Über uns'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage()),);
               },
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Über Petch')),
+        body: Padding(
+          padding: EdgeInsets.all(25),
+          child: 	
+        RichText(
+          text: TextSpan(
+            style: TextStyle(color: Colors.black, fontSize: 36),
+            children: <TextSpan>[
+              TextSpan(text: 'LOGIN', style: TextStyle(fontSize: 80.0))
+        ],
+    ),
+    textScaleFactor: 0.5,
+  ),
+        ));
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Über Petch')),
+        body: Padding(
+          padding: EdgeInsets.all(25),
+          child: 	
+        RichText(
+          text: TextSpan(
+            style: TextStyle(color: Colors.black, fontSize: 36),
+            children: <TextSpan>[
+              TextSpan(text: 'Alles über '),
+              TextSpan(text: 'Petch ', style: TextStyle(color: Colors.blue)),
+              TextSpan(text: 'dot '),
+              TextSpan(text: 'com' , style: TextStyle(decoration: TextDecoration.underline)),
+              TextSpan(text: 'finden Sie genau hier... '),
+              TextSpan(text: 'irgendwann.. ', style: TextStyle(fontSize: 20.0))
+        ],
+    ),
+    textScaleFactor: 0.5,
+  ),
+        ));
+  }
+}
+
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Über Uns')),
+        body: Padding(
+          padding: EdgeInsets.all(25),
+          child: 	
+        RichText(
+          text: TextSpan(
+            style: TextStyle(color: Colors.black, fontSize: 36),
+            children: <TextSpan>[
+              TextSpan(text: 'Dieses Team besteht aus mutigen und '),
+              TextSpan(text: 'übernächtigten ', style: TextStyle(decoration: TextDecoration.underline)),
+              TextSpan(text: 'Mitgliedern aus '),
+              TextSpan(text: 'WI und IW. \n\n'),
+              TextSpan(text: 'Um Genau zu sein aus: \n'),
+              TextSpan(text: 'Tim, Kai und Timucin \n', style: TextStyle(fontSize: 60.0, color: Colors.blue)),
+              TextSpan(text: 'Sie opferten Ihren Schlaf für einpaar Extrapunkte.. ', style: TextStyle(fontSize: 20.0))
+        ],
+    ),
+    textScaleFactor: 0.5,
+  ),
+        ));
   }
 }
